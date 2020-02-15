8da0fd8749195543221615b0c72fafa5421ce383
william.wangmy <877148107@qq.com>
2020-02-15 11:18:07 +0800

    SpringBoot整合shiro框架

--- springboot_integrate_shiro/src/main/java/com/wmy/integrate/shiro/SpringbootIntegrateShiroApplication.java
+++ springboot_integrate_shiro/src/main/java/com/wmy/integrate/shiro/SpringbootIntegrateShiroApplication.java
@@ -1,4 +1,4 @@
-package com.wmy.integrate.mybatis;
+package com.wmy.integrate.shiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
