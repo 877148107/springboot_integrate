587d3b580376a21f89ae26ab4f20d5cc917086d0
william.wangmy <877148107@qq.com>
2020-02-24 22:31:35 +0800

    SpringBoot缓存使用

---
+++ springboot-integrate-cache-redis/src/main/java/com/wmy/integrate/cache/SpringbootIntegrateCacheRedisApplication.java
@@ -0,0 +1,13 @@
+package com.wmy.integrate.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIntegrateCacheRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootIntegrateCacheRedisApplication.class, args);
+    }
+
+}
---
+++ springboot-integrate-cache-redis/src/test/java/com/wmy/integrate/cache/SpringbootIntegrateCacheRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmy.integrate.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntegrateCacheRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
